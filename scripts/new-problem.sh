c7c91f711f3292df9b889bf6f5e5cc1c37d1d721
Justin Kulikauskas <justin.kulikauskas@ibm.com>
2021-01-17 15:31:39 -0500

    Add test scaffold to new problem script

--- scripts/new-problem.sh
+++ scripts/new-problem.sh
@@ -15,3 +15,36 @@ func Solve() {
 	fmt.Println("Hello world")
 }
 EOF
+
+cat > "internal/${problem_name}/main_test.go" <<EOF
+package ${problem_name}
+
+import (
+	"fmt"
+	"testing"
+)
+
+func add(a, b int) int {
+	return a+b
+}
+
+func TestAdd(t *testing.T) {
+	var tests = []struct {
+		a, b int
+		out int
+	}{
+		{1, 2, 3},
+		{2, 2, 4},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("Add %v and %v", tt.a, tt.b)
+		t.Run(testname, func(t *testing.T) {
+			ans := add(tt.a, tt.b)
+			if ans != tt.out {
+				t.Errorf("Got %v, want %v", ans, tt.out)
+			}
+		})
+	}
+}
+EOF
