4420e6411dfd2a98f5fbe321c7dd40d6d2a5c3fd
Justin Kulikauskas <justin.kulikauskas@ibm.com>
2021-01-17 23:35:08 -0500

    Problem 9

---
+++ internal/p009/main.go
@@ -0,0 +1,20 @@
+package p009
+
+import (
+	"fmt"
+)
+
+// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+// Find the product abc.
+func Solve() {
+	for a := 1; a <= 350; a++ {
+		a2 := a * a
+		for b := a + 1; b <= 1000; b++ {
+			c := 1000 - a - b
+			if a2+b*b == c*c {
+				fmt.Println(a * b * c)
+				return
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/JustinKuli/project-euler/internal/p008"
+	"github.com/JustinKuli/project-euler/internal/p009"
 )
 
 func main() {
-	p008.Solve()
+	p009.Solve()
 }
